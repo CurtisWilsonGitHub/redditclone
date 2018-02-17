a734806efd7f114e9b8c102750ff17476a3278a3
Curtis Wilson <curtiswemail@gmail.com>
2018-02-17 08:52:41 -0700

    Start Post Feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,19 +2,34 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
     
-    @posts.each do  |post|
-      if post.id % 5 == 0 || post.id == 1 
-        post.title = 'SPAM'
-      end
-    end
+    # @posts.each do  |post|
+    #   if post.id % 5 == 0 || post.id == 1 
+    #     post.title = 'SPAM'
+    #   end
+    # end
   end
 
   def show
   end
 
   def new
+    @post = Post.new
   end
 
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+    else
+      flash.new[:alert] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
+  
   def edit
   end
 end
