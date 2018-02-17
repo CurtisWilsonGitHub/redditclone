e095ca5686303e1dcce621e4cfeec693f5c767bc
Curtis Wilson <curtiswemail@gmail.com>
2018-02-17 09:01:47 -0700

    Fixed spam filter

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,8 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
     
-    @posts.each do  |post|
-      if post.id % 5 == 0 || post.id == 1 
+    @posts.each_with_index do  |post, index|
+      if index % 5 == 0  
         post.title = 'SPAM'
       end
     end
