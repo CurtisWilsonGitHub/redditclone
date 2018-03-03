09e631da33859509f2a0aa8ef8c240454dfa4f0d
Curtis Wilson <curtiswemail@gmail.com>
2018-03-03 16:40:45 -0700

    Checkpoint21-RailsandPosts

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,18 @@ require 'random_data'
 #create Posts
 Post.find_or_create_by!(title:"First Post Title", body:"First Post Body")
 
+15.times do
+    Topic.create!(
+        name: RandomData.random_sentence,
+        description: RandomData.random_paragraph
+    )
+end
+topics = Topic.all
+
+
 50.times do
     Post.create!(
+        topic: topics.sample,
         title: RandomData.random_sentence,
         body: RandomData.random_paragraph
     )
@@ -31,6 +41,9 @@ Comment.find_or_create_by!( body:"First Comment Body")
     )
 end
 
+
+
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
