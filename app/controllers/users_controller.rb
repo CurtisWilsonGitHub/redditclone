1c24e5c53ab53e2ced742282622700f2782d9713
Curtis Wilson <curtiswemail@gmail.com>
2018-03-08 21:49:38 -0700

    Add confirmation form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
 
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+   
+    helper_method :user_params
+
     def new 
         @user = User.new
     end
@@ -20,5 +26,13 @@ class UsersController < ApplicationController
         end
     end
 
+    def confirm
+        @user = User.new
+        @user.name = params[:user][:name]
+        @user.email = params[:user][:email]
+        @user.password = params[:user][:password]
+        @user.password_confirmation = params[:user][:password_confirmation]
+    end
 
+    
 end
